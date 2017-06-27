e942c6eaaf80e47e39526327509404ff85c1b5e6
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2017-06-27 13:55:42 +0200

    initial

---
+++ spec/atom-julia-lsp-client-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomJuliaLspClientView from '../lib/atom-julia-lsp-client-view';
+
+describe('AtomJuliaLspClientView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
